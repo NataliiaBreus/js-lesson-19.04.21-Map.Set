4ed246b8e42519fa20213c17bd6269c407352440
NataliiaBreus <breusnataly@gmail.com>
2021-04-11 08:48:13 +0300

    initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,64 +1,35 @@
 'use strict';
-function getRandomIntInclusive (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(Math.random() * (max-min+1)) + min;
+class User {
+  constructor (name, surname, year) {
+    this.name = name;
+    this.surname = surname;
+    this.year = year;
+  }
+  set fullName(newFullName) {
+    const splittedFull = newFullNAme.split("");
+    this.name = splittedFullName [0];
+    this.surname = splittedFullName[1];
+  }
+  get fullName(){
+    return `${this.name} ${this.surname}`;
+  }
+  static isUser(user){
+    return user instanceof User;
+  }
 }
 
-const numsArr = [];
-for (let i = 0; i<10; i++){
-  numsArr.push(getRandomIntInclusive(100, 200));
+class Student extends User {
+  constructor (name, surname, year){
+    super (name, surname, year)
+  }
+  
+  getCourse(){
+    let date = new Date();
+    return date - this.year;
+  }
 }
+const u = new User ('Mark', 'Smith', 2018);
+const s = new Student ('Ivan', 'Ivanov', 2020);
+console.log(User);
+console.log(Student);
 
-function User(firstName, lastName, email, age, isMale, isSubscribed = false){
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.email = email;
-  this.age = age;
-  this.isMale = isMale;
-  this.isSubscribed = isSubscribed;
-}
-const NATIONALITIES = [
-  'uk',
-  'us',
-  'bg',
-  'ru',
-  'uk',
-  'fr',
-]
-
-User.prototype = new UserProto();
-function UserProto(){
-this.getFullName = function(){
-  return `${this.firstName} ${this.lastName}`;
-}
-}
- function getRandomUsers (amount){
-   const usersArray = [];
-   for (let i = 0; i < amount; i++){
-     usersArray.push(
-       new User(
-         `Name${i}`, //first name
-         `Surname${i}`, // last name
-         `user${i}@gmail.com`, //email
-         getRandomIntInclusive(10, 50), // age
-         getRandomNationality(),
-         Math.random() < 0.5, // isMale
-         Math.random() <0.5 // isSubscribed
-       )
-     );
-   }
-   return usersArray;
- }
-
- function getRandomNationality(){
-   return NATIONALITIES [getRandomIntInclusive(0,NATIONALITIES.length - 1)];
- }
-
- const userList = getRandomUsers(100);
-
- function getSubscribedAdultWoman(arr){
- return arr.filter(function(user){
-  return user.age >= 18 && !user.isMale && user.isSubscribed;
-});
- }
- 
