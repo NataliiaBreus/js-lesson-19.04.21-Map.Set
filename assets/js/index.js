82a73fda27544fc057c96715c51c44500cd68683
NataliiaBreus <breusnataly@gmail.com>
2021-04-12 07:00:40 +0300

    changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,16 @@ class User {
     this.surname = surname;
     this.year = year;
   }
+  set year (newYear) {
+    if (typeof newYear !== "number"){
+      throw new TypeError();
+    }
+    this._year = newYear;
+  }
+  get year(){
+    return this._year;
+  }
+
   set fullName(newFullName) {
     const splittedFull = newFullNAme.split("");
     this.name = splittedFullName [0];
@@ -17,19 +27,29 @@ class User {
     return user instanceof User;
   }
 }
-
 class Student extends User {
+
   constructor (name, surname, year){
     super (name, surname, year)
   }
   
   getCourse(){
-    let date = new Date();
-    return date - this.year;
+    
   }
 }
+
+
+let date = new Date();
+let yearNow = date.getFullYear();
+let numOfCourse = yearNow - this.year;
+  if (numOfCourse < 1 || numOfCourse > 5){
+    throw new TypeError ("Wrong number!");
+  }
+  console.log(numOfCourse);
+
+
+
 const u = new User ('Mark', 'Smith', 2018);
 const s = new Student ('Ivan', 'Ivanov', 2020);
 console.log(User);
 console.log(Student);
-
