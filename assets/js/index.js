7ed1fef681faf7ba154220975c4384ed9a8eef8f
Nataliia Breus <breusnataly@gmail.com>
2021-04-14 11:50:01 +0300

    changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,175 @@
 'use strict';
-class User {
-  constructor (name, surname) {
-    this.name = name;
-    this.surname = surname;
-  }
 
-  set fullName(newFullName) {
-    const splittedFull = newFullNAme.split("");
-    this.name = splittedFullName [0];
-    this.surname = splittedFullName[1];
-  }
-  get fullName(){
-    return `${this.name} ${this.surname}`;
-  }
-  static isUser(user){
-    return user instanceof User;
-  }
-}
-class Student extends User {
+// 4 Создать классы
 
-  constructor (name, surname, year){
-    super (name, surname);
-    this.year = year;
-  }
-  set year (newYear) {
-    if (typeof newYear !== "number"){
-      throw new TypeError();
+class Book {
+    constructor (author, name, year, publishingHouse) {
+        this.author = author;
+        this.name = name;
+        this.year = year;
+        this.publishingHouse = publishingHouse; 
     }
-    this._year = newYear;
-  }
-  get year(){
-    return this._year;
-  }
-  get course(){
-    const yearNow = new Date().getFullYear();
-    const numOfCourse = yearNow - this.year + 1;
-    if (numOfCourse < 1 || numOfCourse > 5){
-      throw new RangeError ("Wrong number!");
+    set author (newAuthor){
+        if (typeof newAuthor !== 'string'){
+            throw new TypeError();
+        }
+        this._author = newAuthor;
     }
-  }
+    get author() {
+        return this._author;
+    }
+    set name (newName){
+        if (typeof newName !== 'string'){
+            throw new TypeError();
+        }
+        this._name = newName;
+    }
+    get name() {
+        return this._name;
+    }
+    set year (newYear){
+        if (typeof newYear !== 'number'){
+            throw new TypeError();
+        }
+        this._year = newYear;
+    }
+    get year() {
+        return this._year;
+    }
+    set publishingHouse (newPublishingHouse){
+        if (typeof newPublishingHouse !== 'string'){
+            throw new TypeError();
+        }
+        this._publishingHouse = newPublishingHouse;
+    }
+    get publishingHouse() {
+        return this._publishingHouse;
+    }
+}
+class ElectronicBook extends Book {
+    constructor (author, name, year, publishingHouse, format, elNumber) {
+        super (author, name, year, publishingHouse);
+
+        this.format = format;
+        this.elNumber = elNumber;
+    }
+    set format (newFormat){
+        if (typeof newFormat !== 'string') {
+            throw new TypeError();
+        }
+        this._format = newFormat;
+    }
+    get format(){
+        return this._format;
+    }
+
+}
+const b = new Book ('Mark Tven', 'Tom Soyer', 2019, 'Svet');
+const elB = new ElectronicBook ('Pushkin', 'Evgeniy Onegin', 1995, 'Ranok', 'elBook', '12345');
+console.log(b);
+console.log (elB);
+
+// 2 Реализовать с использованием классов. Для каждого поля создать get set с проверкой типов
+
+class Student {
+    constructor (name, surname, isMale, contactInfo, univerInfo) {
+        this.name = name;
+        this.surname = surname;
+        this.isMale = isMale;
+        this.contactInfo = contactInfo;
+        this.univerInfo = univerInfo;
+    }
+    set name (newName){
+        if (typeof newName !== 'string') {
+
+            throw new TypeError();
+        }
+        this._name = newName;
+    }
+    get name() {
+        return this._name;
+    }
+    set surname (newSurname){
+        if (typeof newSurname !== 'string') {
+
+            throw new TypeError();
+        }
+        this._surname = newSurname;
+    }
+    get surname() {
+        return this._surname;
+    }
+    set isMale (newIsMale){
+        if (typeof newIsMale !== 'boolean') {
+
+            throw new TypeError();
+        }
+        this._isMale = newIsMale;
+    }
+    get isMale() {
+        return this._isMale;
+    }
+    set contactInfo (newContactInfo){
+        if (typeof newContactInfo !== 'string') {
+
+            throw new TypeError();
+        }
+        this._contactInfo = newContactInfo;
+    }
+    get contactInfo() {
+        return this._contactInfo;
+    }
+
+    get faculty() {
+        return this._univerInfo.faculty;
+    }
+    get department () {
+        return this._univerInfo.department;
+    }
 }
 
+class UniverInfo {
+    constructor (faculty, department) {
+        this.faculty = faculty;
+        this.department = department;
+    }
+    set faculty (newFaculty){
+        if (typeof newFaculty !== 'string'){
+            throw new TypeError();
+        }
+        this._faculty = newFaculty;
+    }
+    set department (newDepartment){
+        if (typeof newDepartment !== 'string'){
+            throw new TypeError();
+        }
+        this._departmnet = newDepartment;
+    }
+}
+const ui = new UniverInfo ('management', 'Organization Management');
 
-const u = new User ('Mark', 'Smith');
-const s = new Student ('Ivan', 'Ivanov', 2020);
-console.log(User);
-console.log(Student);
+const stud1 = new Student ('Mark', 'Smith', true, 'contact info',ui);
+const stud2 = new Student ('Alica', 'Ivanova', false, 'contacts', ui)
+
+console.log (stud1);
+console.log (stud2);
+console.log (ui);
+
+// 1 Вычислить сумму первых п элементов последовательности. Параметр N задает пользователь
+
+let sumCounter = 0;
+const number = +prompt ("Write a number:");
+let sum = 0;
+
+while(sumCounter < number) {
+    sumCounter ++;
+    sum += sumCounter;
+}
+console.log (sum);
+
+
+// 3 Создать числовой массив и проинициализировать его из 25 элементов
+
+const numArray = [];
+
+
